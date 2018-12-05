692a3ac4b6c4070333cba401cb7c9018dd3cfd2a
Ahmed Zaher <adzr@users.noreply.github.com>
2018-12-06 00:52:44 +0200

    Updating copyrights.

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Ahmed Zaher
+Copyright 2018 Ahmed Zaher
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
